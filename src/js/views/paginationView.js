c2a20999f62658710f0fd07acc4e82738fe0cf01
kennethnnalue <kennethnnalue@gmail.com>
2022-03-02 04:11:30 +0800

    bugs

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -1,5 +1,5 @@
 import icons from 'url:../../img/icons.svg';
-import View from './View';
+import View from './View.js';
 
 class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
